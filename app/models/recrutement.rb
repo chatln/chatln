c1469692c48de1d3c6c40da34cebbf3fe35584ab
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-08-05 12:30:20 +0000

    build form, edit, Analytic: add Improvely

--- app/models/recrutement.rb
+++ app/models/recrutement.rb
@@ -1,2 +1,7 @@
 class Recrutement < ApplicationRecord
+
+  #COSTANTE
+STATUS= ["Personnel", "Prof Partenaire"]
+ROLE= ["City Manager", "Professeur Partenaire"]
+CITY= ["Tiassalé", "N'Douci","Agboville","Odienné","Divo", "Kouto","Man","Bouaké","Yamoussokro","Autres villes"]
 end
